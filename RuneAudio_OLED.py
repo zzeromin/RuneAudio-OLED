9c184847d05eacf13bf7a6c4c14bb35810f33044
Youngmin Seo <zzeromin@gmail.com>
2018-06-18 14:28:09 +0900

    Update RuneAudio_OLED.py

--- RuneAudio_OLED.py
+++ RuneAudio_OLED.py
@@ -5,34 +5,39 @@
 Title        : RuneAudio_OLED.py
 Author       : zzeromin, member of Raspberrypi Village and Tentacle Team
 Creation Date: Apr 30, 2017
-Blog         : http://rasplay.org, http://forums.rasplay.org/, https://zzeromin.tumblr.com/
+Blog         : http://rasplay.org, http://forums.rasplay.org/
 Github       : https://github.com/rasplay, https://github.com/zzeromin
 Cafe         : http://cafe.naver.com/raspigamer
 Thanks to    : smyani
 Free and open for all to use. But put credit where credit is due.
 
 reference    :
-    https://brunch.co.kr/@gogamza/6
-    https://github.com/haven-jeon/piAu_volumio
-    http://blog.naver.com/kjnam100/220805352857
-    https://pypi.python.org/pypi/Pillow/2.1.0
+ https://brunch.co.kr/@gogamza/6
+ https://github.com/haven-jeon/piAu_volumio
+ http://blog.naver.com/kjnam100/220805352857
+ https://pypi.python.org/pypi/Pillow/2.1.0
 
-installed python package: python2-pip, i2c-tools, python-imaging, python-mpd2, gcc
-This code edited for rpi2 runeaudio by zzeromin
+ installed python package: python2-pip, i2c-tools, python-imaging, python-mpd2, gcc
+ This code edited for rpi2 runeaudio by zzeromin
 
 install      :
-    pacman -Syy
-    pacman -S python2-pip 
-    pacman -S gcc
-    pacman -S python-imaging
-    pip install python-mpd2
-    git clone https://github.com/adafruit/Adafruit_Python_GPIO.git
-    git clone https://github.com/adafruit/Adafruit_Python_SSD1306.git
-    git clone https://github.com/zzeromin/RuneAudio-OLED.git
+pacman -Syy
+pacman -S python2-pip
+pacman -S gcc
+pacman -S python2-imaging
+pip install python-mpd2
+git clone https://github.com/adafruit/Adafruit_Python_GPIO.git
+git clone https://github.com/adafruit/Adafruit_Python_SSD1306.git
+git clone https://github.com/zzeromin/RuneAudio-OLED.git
 """
 
 import time
-import Adafruit_GPIO.SPI as SPI
+import os
+from sys import exit
+from subprocess import *
+from time import *
+from datetime import datetime
+#import Adafruit_GPIO.SPI as SPI
 import Adafruit_SSD1306
 
 from PIL import Image
@@ -127,17 +132,15 @@ class MPDPoller(object):
 
             if stats['state'] == 'stop':
                 return(None)
-#            print(stats)
-#            print(song)
+
             if 'artist' not in song:
-#                artist = ""
                 artist = song['name']
             else:
                 artist = song['artist']
-            
+
             if 'title' not in song:
                 title = ""
-            else:    
+            else:
                 title = song['title']
 
             song_info = ""
@@ -146,19 +149,22 @@ class MPDPoller(object):
                 audio = ""
             else:
                 frequency = stats['audio'].split(':')[0]
-                z, f = divmod( int(frequency), 1000 )
-                if ( f == 0 ): frequency = str(z) + " kHz"
-                else: frequency = str( float(frequency) / 1000 ) + "kHz"
+                z, f = divmod(int(frequency), 1000)
+                if (f == 0): frequency = str(z) + " kHz"
+                else: frequency = str(float(frequency) / 1000) + "kHz"
                 song_info += stats['audio'].split(':')[1] + "bit " + frequency
 
             bitrate = stats['bitrate']
             song_info += " " + stats['bitrate'] + "kbps"
 
+#            total_time = song['time']
             songplayed = stats['elapsed']
-            vol = stats['volume']
             m, s = divmod(float(songplayed), 60)
+#            m, s = divmod(float(elapsed_time), 60)
             h, m = divmod(m, 60)
-            
+
+            vol = stats['volume']
+
 
         # Couldn't get the current song, so try reconnecting and retrying
         except (MPDError, IOError):
@@ -187,12 +193,26 @@ class MPDPoller(object):
 #        print(song_info)
 #        print(audio)
         eltime = "%d:%02d:%02d" % (h, m, s)
-        return({'artist':artist, 'title':title, 'eltime':eltime, 'volume':int(vol), 'song_info':song_info})
+        return({'artist': artist, 'title': title, 'eltime': eltime, 'volume': int(vol), 'song_info': song_info})
 
+def run_cmd(cmd):
+    # runs whatever is in the cmd variable in the terminal
+    p = Popen(cmd, shell=True, stdout=PIPE)
+    output = p.communicate()[0]
+    return output
 
+def get_ip_address(cmd, cmdeth):
+    # ip & date information
+    ipaddr = run_cmd(cmd)
+
+    # selection of wlan or eth address
+    count = len(ipaddr)
+    if count == 0 :
+        ipaddr = run_cmd(cmdeth)
+    return ipaddr
 
 def main():
-    from time import sleep
+
     # Initialize library.
     disp.begin()
 
@@ -210,7 +230,7 @@ def main():
     draw = ImageDraw.Draw(image)
 
     # Draw a black filled box to clear the image.
-    draw.rectangle((0,0,width,height), outline=0, fill=0)
+    draw.rectangle((0, 0, width, height), outline=0, fill=0)
 
     # Draw some shapes.
     # First define some constants to allow easy resizing of shapes.
@@ -222,58 +242,85 @@ def main():
     # Load default font.
     poller = MPDPoller()
     poller.connect()
-    font_art = ImageFont.truetype('/root/RuneAudio_OLED.py/NanumGothic.ttf', 13)
-    font_tit = ImageFont.truetype('/root/RuneAudio_OLED.py/NanumGothic.ttf', 13)
-    font_info = ImageFont.truetype('/root/RuneAudio_OLED.py/NanumGothic.ttf', 12)
+    font_art = ImageFont.truetype('/root/RuneAudio_OLED/NanumGothic.ttf', 13)
+    font_tit = ImageFont.truetype('/root/RuneAudio_OLED/NanumGothic.ttf', 13)
+    font_info = ImageFont.truetype('/root/RuneAudio_OLED/NotoSansUI-Regular.ttf', 12)
+    #get ip address of eth0 connection
+    cmdeth = "ip addr show eth0 | grep 'inet ' | awk '{print $2}' | cut -d/ -f1"
+    #get ip address of wlan0 connection
+    cmd = "ip addr show wlan0 | grep 'inet ' | awk '{print $2}' | cut -d/ -f1"
+    #cmd = "ip addr show wlan1 | grep 'inet ' | awk '{print $2}' | cut -d/ -f1"
+
+    #get ip address of eth0 connection    
+    ipaddr = get_ip_address(cmd, cmdeth)
 
     while True:
-        draw.rectangle((0,0,width,height), outline=0, fill=0)
+        draw.rectangle((0, 0, width, height), outline=0, fill=0)
         status = poller.poll()
         if status is None:
-            draw.rectangle((0,0,width,height), outline=0, fill=0)
+            draw.rectangle((0, 0, width, height), outline=0, fill=0)
+            msg1 = "Welcome RuneAudio"
+            msg2 = "RAS Yolo Pi"
+            ipaddr = get_ip_address(cmd, cmdeth)
+            draw.text((0, top), unicode(msg1).center(1,' '), font=font_tit, fill=255)
+            draw.text((0, top+15), unicode(msg2).center(2, ' '), font=font_tit, fill=255)
+            draw.text((0, top+30), datetime.now().strftime( "%b %d %H:%M:%S" ), font=font_info, fill=255)
+            draw.text((0, top+45), "IP " + ipaddr, font=font_info, fill=255)
+            disp.image(image)
+            disp.display()
             continue
+
         artist = status['artist']
         title = status['title']
         eltime = status['eltime']
         vol = status['volume']
         song_info = status['song_info']
-#        print artist
-#        print title
-#        print eltime
-#        print vol
+        print eltime
+
         div_vol = vol // 10
         vol_str = ""
         for i in range(div_vol):
-            vol_str =vol_str + '+' 
+            vol_str = vol_str + '+'
         for i in range(10 - div_vol):
             vol_str = vol_str + '-'
-        
-        title_length = len(title)
+
+        titleLength = len(title)
+        txtFind = title[0]
+        if txtFind.isalpha():
+            titleLine3 = 40
+            titleLine2 = 22
+            titleIndex1 = 17
+            titleIndex2 = 35
+        else:
+            titleLine3 = 60
+            titleLine2 = 24
+            titleIndex1 = 16
+            titleIndex2 = 32
+
+        print (titleLength, txtFind.isalpha())
+
         title = unicode(title)
-        print title_length
 
-        if title_length > 40:
-            draw.text((0, top), unicode(artist).center(24,' '), font=font_art, fill=255)
-            draw.text((0, top+13), title[0:16], font=font_tit, fill=255)
-            draw.text((0, top+27), title[16:32], font=font_tit, fill=255)
-            draw.text((0, top+41), title[32:48], font=font_tit, fill=255)
-            #draw.text((0, top+), song_info, font=font_info, fill=255)
+        if titleLength > titleLine3:
+            draw.text((0, top), unicode(artist), font=font_art, fill=255)
+            draw.text((0, top+15), title[0:titleIndex1], font=font_tit, fill=255)
+            draw.text((0, top+31), title[titleIndex1:titleIndex2], font=font_tit, fill=255)
+            draw.text((0, top+46), title[titleIndex2:60], font=font_tit, fill=255)
 
-        elif title_length > 20:
-            draw.text((0, top), unicode(artist).center(24,' '), font=font_art, fill=255)
-            draw.text((0, top+13), title[0:16], font=font_tit, fill=255)
-            draw.text((0, top+27), title[16:32], font=font_tit, fill=255)
-#            draw.text((0, top+41), eltime, font=font_info, fill=255)
-#            draw.text((86, top+41),"Vol " +  str(vol) , font=font_info, fill=255)
-            draw.text((0, top+41), song_info, font=font_info, fill=255)
+        elif titleLength > titleLine2:
+            draw.text((0, top), unicode(artist), font=font_art, fill=255)
+            draw.text((0, top+15), title[0:titleIndex1], font=font_tit, fill=255)
+            draw.text((0, top+32), title[titleIndex1:titleIndex2], font=font_tit, fill=255)
+            draw.text((0, top+46), eltime, font=font_info, fill=255)
+            draw.text((90, top+46), "Vol " + str(vol), font=font_info, fill=255)
+#            draw.text((0, top+45), song_info, font=font_info, fill=255)
 
         else:
-            draw.text((0, top), unicode(artist).center(24,' '), font=font_art, fill=255)
-            draw.text((0, top+15), unicode(title).center(24, ' '), font=font_tit, fill=255)
-            draw.text((0, top+30), eltime, font=font_info, fill=255)
-            draw.text((86, top+30),"Vol " +  str(vol) , font=font_info, fill=255)
+            draw.text((0, top), unicode(artist), font=font_art, fill=255)
+            draw.text((0, top+17), unicode(title), font=font_tit, fill=255)
+            draw.text((0, top+32), eltime, font=font_info, fill=255)
+            draw.text((90, top+32), "Vol " + str(vol), font=font_info, fill=255)
             draw.text((0, top+45), song_info, font=font_info, fill=255)
-#        draw.text((0, top+45), vol_str, font=font_tm, fill=255)
 
         disp.image(image)
         disp.display()
